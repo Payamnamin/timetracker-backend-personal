e72c9a02b7e7a526fb0ad7fd22ad39b53fd10927
Payam <payamnamin@programmer.net>
2026-05-01 20:41:18 +0200

    Add Get Mapping and Postmapping on Categ Controller

--- src/main/java/com/example/demo/controller/CategoryController.java
+++ src/main/java/com/example/demo/controller/CategoryController.java
@@ -1,8 +1,15 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.model.Category;
 import com.example.demo.service.CategoryService;
 
 @RestController
@@ -15,5 +22,14 @@ public class CategoryController {
         this.service = service;
     }
 
-    
+    @PostMapping 
+    public Category Create(@RequestBody Category category) {
+        return service.createCategory(category);
+    }
+
+    @GetMapping
+    public List<Category> getAll(@RequestParam String userId) {
+        return service.getAllCategories(userId);
+
+    }
 }
