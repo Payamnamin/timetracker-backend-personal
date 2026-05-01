7cd03f98b52ac64fcc599a8eeeef9ba818aa89f1
Payam <payamnamin@programmer.net>
2026-05-01 19:33:36 +0200

    Add Category model

---
+++ src/main/java/com/example/demo/model/Category.java
@@ -0,0 +1,48 @@
+package com.example.demo.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collation="categories")
+
+public class Category {
+    
+    @Id
+    private String id;
+    private String name;
+    private String userId;
+
+    public Category(String id, String name, String userId) {
+        this.id = id;
+        this.name = name;
+        this.userId = userId;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+
+    
+    
+}
