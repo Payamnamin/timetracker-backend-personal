333d1729778848facb03c92c4918acdb1b2f8d9f
Payam <payamnamin@programmer.net>
2026-05-01 20:31:10 +0200

    Add basic Categ Controller

---
+++ src/main/java/com/example/demo/controller/CategoryController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.service.CategoryService;
+
+@RestController
+@RequestMapping("/api/categories")
+public class CategoryController {
+
+    private final CategoryService service;
+
+    public CategoryController(CategoryService service) {
+        this.service = service;
+    }
+
+    
+}
