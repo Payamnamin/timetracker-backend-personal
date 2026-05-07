e64e1898ac82e9d19ac82b7d1eddd78e588bce69
Payam <payamnamin@programmer.net>
2026-05-07 15:00:19 +0200

    Created TimeEntry model

---
+++ src/main/java/com/example/demo/model/TimeEntry.java
@@ -0,0 +1,73 @@
+package com.example.demo.model;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "timeEntries")
+public class TimeEntry {
+
+    @Id
+    private String id;
+    private String userId;
+    private String categoryId;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+
+    public TimeEntry() {
+
+    }
+
+    public TimeEntry(String id, String userId, String categoryId, LokalDateTime startTime, LocalDateTime endTime) {
+        this.id = id;
+        this.userId = userId;
+        this.categoryId = categoryId;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(String categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalDateTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalDateTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalDateTime endTime) {
+        this.endTime = endTime;
+    }
+
+    
+    
+}
