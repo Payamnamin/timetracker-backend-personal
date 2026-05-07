abd2472bd5231550b72435d3c396c6069fb3a4c3
Payam <payamnamin@programmer.net>
2026-05-07 16:01:54 +0200

    Added create Time entry endpoint

--- src/main/java/com/example/demo/controller/TimeEntryController.java
+++ src/main/java/com/example/demo/controller/TimeEntryController.java
@@ -1,8 +1,11 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.model.TimeEntry;
 import com.example.demo.service.TimeEntryService;
 
 @RestController
@@ -13,5 +16,10 @@ public class TimeEntryController {
 
     public TimeEntryController(TimeEntryService service) {
         this.service = service;
+    }
+
+    @PostMapping
+    public TimeEntry createTimeEntry(@RequestBody TimeEntry timeEntry) {
+        return service.createTimeEntry(timeEntry);
     }
 }
