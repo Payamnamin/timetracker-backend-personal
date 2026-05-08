8bb64dd5acf9e60bb38ddadfafa799d3e44bec0f
Payam <payamnamin@programmer.net>
2026-05-08 09:04:01 +0200

    Add CrossOrgin on time entry controller

--- src/main/java/com/example/demo/controller/TimeEntryController.java
+++ src/main/java/com/example/demo/controller/TimeEntryController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.TimeEntry;
 import com.example.demo.service.TimeEntryService;
-
+@CrossOrigin(origins="*")
 @RestController
 @RequestMapping("/time-entries")
 public class TimeEntryController {
